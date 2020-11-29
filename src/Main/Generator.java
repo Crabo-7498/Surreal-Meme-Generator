cd2c32e09fc1e712c0749db2f80b83a5f01b2e3b
Crabo <vincent.qi@hotmail.com>
2020-11-28 21:20:02 -0800

    Initial Commit

---
+++ src/Main/Generator.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class Generator {
+    public static void main(String[] args) {
+
+    }
+
+    public void Generate() {
+
+    }
+
+    private static void DrawImages() {
+
+    }
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class Main {
+    public static void main(String[] args) {
+        Generator g = new Generator();
+        g.Generate();
+    }
+}
