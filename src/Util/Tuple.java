a4211e55da11fd566a138d40a9dfa938a9fdc118
Crabo <vincent.qi@hotmail.com>
2020-12-04 20:35:40 -0800

    Refactored Variables

--- src/Util/Tuple.java
+++ src/Util/Tuple.java
@@ -1,9 +1,9 @@
 package Util;
 
-public class Tuple<XT, YT> {
-    public final XT x;
-    public final YT y;
-    public Tuple(XT x, YT y) {
+public class Tuple<X, Y> {
+    public final X x;
+    public final Y y;
+    public Tuple(X x, Y y) {
         this.x = x;
         this.y = y;
     }
