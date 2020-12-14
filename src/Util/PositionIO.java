5f96c00418f09144f8cbeba2dbd221ffd03171a5
= <vincent.qi@hotmail.com>
2020-12-14 15:59:01 -0800

    Added Documentation

--- src/Util/PositionIO.java
+++ src/Util/PositionIO.java
@@ -3,10 +3,19 @@ package Util;
 import java.awt.image.BufferedImage;
 import java.util.Random;
 
+/**
+ * The PositionIO class generates random positions for items
+ */
 public class PositionIO {
     Random r = new Random();
 
-    public Tuple<Integer, Integer> GetRandomPosition(BufferedImage image, Tuple<Integer, Integer> bounds) {
+    /**
+     * Generates a random position for an Image inside Bounds
+     * @param image The image to be generated
+     * @param bounds The bounds of the background
+     * @return Returns a random position x,y in the form of a Tuple
+     */
+    public Tuple<Integer, Integer> GetRandomPositionForImage(BufferedImage image, Tuple<Integer, Integer> bounds) {
         return new Tuple<>(
                 r.nextInt(Math.max(bounds.x - image.getWidth(), 0)),
                 r.nextInt(Math.max(bounds.y - image.getHeight(), 0))
