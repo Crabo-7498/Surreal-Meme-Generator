f32a823c32c4fb8f56a2c035ab09e41a5f444ecc
= <vincent.qi@hotmail.com>
2020-12-14 16:12:36 -0800

    Updated PositionIO

--- src/Util/PositionIO.java
+++ src/Util/PositionIO.java
@@ -11,6 +11,7 @@ public class PositionIO {
 
     /**
      * Generates a random position for an Image inside Bounds
+     *
      * @param image The image to be generated
      * @param bounds The bounds of the background
      * @return Returns a random position x,y in the form of a Tuple
@@ -19,6 +20,19 @@ public class PositionIO {
         return new Tuple<>(
                 r.nextInt(Math.max(bounds.x - image.getWidth(), 0)),
                 r.nextInt(Math.max(bounds.y - image.getHeight(), 0))
+        );
+    }
+
+    /**
+     * Generates a random position for Text inside bounds
+     *
+     * @param bounds The bounds of the background
+     * @return Returns a random position x,y in the form of a Tuple
+     */
+    public Tuple<Integer, Integer> GetRandomPositionForText(Tuple<Integer, Integer> bounds) {
+        return new Tuple<>(
+                r.nextInt(Math.min(Math.max(bounds.x, bounds.x / 10), bounds.x / 2)),
+                r.nextInt((int) Math.min(Math.max(bounds.y, bounds.y / 10), bounds.y * 0.9f))
         );
     }
 }
