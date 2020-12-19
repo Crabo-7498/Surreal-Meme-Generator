21b3b8fb7e4970ca57a3aba10460b9865d93c164
= <vincent.qi@hotmail.com>
2020-12-18 16:44:04 -0800

    Updated PositionIO

--- src/Main/Generator.java
+++ src/Main/Generator.java
@@ -6,6 +6,7 @@ import ResourceIO.ImageLoader;
 import TextIO.ConfigEditor;
 import TextIO.SentenceCreator;
 import TextIO.TextParser;
+import Util.PositionIO;
 import Util.Tuple;
 
 import java.awt.image.BufferedImage;
@@ -23,12 +24,15 @@ public class Generator {
             FileIO fIO = new FileIO();
             cfgEditor.WriteConfig();
 
-            System.out.println(fIO.GetRandomFile("Resources/Images/Surreal"));
-            //imgEdit.AddImage(imgLoad.LoadImage("Test/t2.jpg"), new Tuple<Integer, Integer>(500, 500), new Tuple<Integer, Integer>(10, 10));
+            //System.out.println(fIO.GetRandomFile("Resources/Images/Surreal"));
+            BufferedImage loadedImage = imgLoad.LoadImage("Surreal/" + fIO.GetRandomFile("Resources/Images/Surreal"));
+            Tuple<Integer, Integer> size = new Tuple<Integer, Integer>(500, 500);
+            imgEdit.AddImage(loadedImage, size, PositionIO.GetRandomPositionForImage(new Tuple<>(bg.getWidth() - size.x, bg.getHeight() - size.y)));
+
             //imgEdit.AddImage(imgLoad.LoadImage("Test/t6.jpg"), new Tuple<Integer, Integer>(500, 500), new Tuple<Integer, Integer>(70, 70));
             //imgEdit.AddText(bg, stnCreate.CreateSentence(), new Tuple<String, Float>("mn.ttf", 60f), new Tuple<Integer, Integer>(100, 300));
             //imgEdit.AddText(bg, txtParser.RandomWord(null, "Nouns"), new Tuple<String, Float>("mn.ttf", 1000f), new Tuple<Integer, Integer>(500, 700));
-            //imgLoad.SaveImage("Export.png", bg);
+            imgLoad.SaveImage("Export.png", bg);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/Util/PositionIO.java
+++ src/Util/PositionIO.java
@@ -7,19 +7,18 @@ import java.util.Random;
  * The PositionIO class generates random positions for items
  */
 public class PositionIO {
-    Random r = new Random();
+    static Random r = new Random();
 
     /**
      * Generates a random position for an Image inside Bounds
      *
-     * @param image The image to be generated
      * @param bounds The bounds of the background
      * @return Returns a random position x,y in the form of a Tuple
      */
-    public Tuple<Integer, Integer> GetRandomPositionForImage(BufferedImage image, Tuple<Integer, Integer> bounds) {
+    public static Tuple<Integer, Integer> GetRandomPositionForImage(Tuple<Integer, Integer> bounds) {
         return new Tuple<>(
-                r.nextInt(Math.max(bounds.x - image.getWidth(), 0)),
-                r.nextInt(Math.max(bounds.y - image.getHeight(), 0))
+                r.nextInt(Math.max(bounds.x, 1)),
+                r.nextInt(Math.max(bounds.y, 1))
         );
     }
 
@@ -29,7 +28,7 @@ public class PositionIO {
      * @param bounds The bounds of the background
      * @return Returns a random position x,y in the form of a Tuple
      */
-    public Tuple<Integer, Integer> GetRandomPositionForText(Tuple<Integer, Integer> bounds) {
+    public static Tuple<Integer, Integer> GetRandomPositionForText(Tuple<Integer, Integer> bounds) {
         return new Tuple<>(
                 r.nextInt(Math.min(Math.max(bounds.x, bounds.x / 10), bounds.x / 2)),
                 r.nextInt((int) Math.min(Math.max(bounds.y, bounds.y / 10), bounds.y * 0.9f))
